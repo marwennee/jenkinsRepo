276366423687b81cdcdfce8d147ab813c3bbae16
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-08-17 16:56:21 +0100

    remove step

--- seed.groovy
+++ seed.groovy
@@ -1,9 +1,5 @@
 def script_back_1 = readFileFromWorkspace('./back_1.groovy')
 def script_back_2 = readFileFromWorkspace('./back_2.groovy')
 
-job('back-1') {
-    steps {
-        groovyCommand(script_back_1)
-        groovyCommand(script_back_2)
-    }
-}
+groovyCommand(script_back_1)
+groovyCommand(script_back_2)
