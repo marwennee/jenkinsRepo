cf1700ac82fa47b60dee5767357b5d12caa3c733
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-08-17 16:42:53 +0100

    groovy

--- seed.groovy
+++ seed.groovy
@@ -1,8 +1,6 @@
 def releaseScript = readFileFromWorkspace('./back_1.groovy')
 job('back-1') {
     steps {
-        def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
-        scriptApproval.approveScript(scriptApproval.hash(pipelineScript, 'groovy'))
-        script releaseScript
+        groovyCommand(releaseScript)
     }
 }
