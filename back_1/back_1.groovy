53ce290586a95a1644bda6d7beddf4648b54441b
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-08-18 10:24:36 +0100

    structure code

---
+++ back_1/back_1.groovy
@@ -0,0 +1,27 @@
+# groovy file
+pipelineJob('back_service_1') {
+  properties{
+    githubProjectUrl('https://github.com/marwennee/back-service-1')
+    pipelineTriggers{
+      triggers {
+        githubPush()
+      }
+    }
+    buildDiscarder {
+      strategy {
+          logRotator {
+              daysToKeepStr("5")
+              numToKeepStr("3")
+              artifactDaysToKeepStr("0")
+              artifactNumToKeepStr("0")
+          }
+      }
+    }
+  }
+  definition {
+    cps {
+      script(readFileFromWorkspace('./Jenkinsfile-back-1'))
+      sandbox()    
+    }
+  }
+}
--- seed.groovy
+++
@@ -1,5 +0,0 @@
-def script_back_1 = readFileFromWorkspace('./back_1.groovy')
-def script_back_2 = readFileFromWorkspace('./back_2.groovy')
-
-groovyCommand(script_back_1)
-groovyCommand(script_back_2)
