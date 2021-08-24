d4c9ab7b82ac1a710e6c3dad1209abc9ef8a30b0
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-08-24 10:04:53 +0100

    helm

---
+++ helm.groovy
@@ -0,0 +1,26 @@
+pipelineJob('helm') {
+  properties{
+    githubProjectUrl('https://github.com/marwennee/helm.git')
+    pipelineTriggers{
+      triggers {
+        githubPush()
+      }
+    }
+    buildDiscarder {
+      strategy {
+          logRotator {
+              daysToKeepStr("5")
+              numToKeepStr("3")
+              artifactDaysToKeepStr("0")
+              artifactNumToKeepStr("0")
+          }
+      }
+    }
+  }
+  definition {
+    cps {
+      script(readFileFromWorkspace('./Jenkinsfile_helm'))
+      sandbox()    
+    }
+  }
+}
